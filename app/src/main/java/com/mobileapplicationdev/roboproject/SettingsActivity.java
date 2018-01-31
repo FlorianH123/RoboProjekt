876621e591494226f9aece1e11ff7600f103ed83
FlorianH123 <f.hennrich@gmx.de>
2018-01-31 16:53:50 +0100

    refactor

--- app/src/main/java/com/mobileapplicationdev/roboproject/SettingsActivity.java
+++ app/src/main/java/com/mobileapplicationdev/roboproject/SettingsActivity.java
@@ -3,7 +3,6 @@ package com.mobileapplicationdev.roboproject;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
