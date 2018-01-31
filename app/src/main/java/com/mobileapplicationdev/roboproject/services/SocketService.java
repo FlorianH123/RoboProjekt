97977ddba0d1fd4de540d5b9b4d17b8b0a6ee635
FlorianH123 <f.hennrich@gmx.de>
2018-01-31 17:09:57 +0100

    refactor

--- app/src/main/java/com/mobileapplicationdev/roboproject/services/SocketService.java
+++ app/src/main/java/com/mobileapplicationdev/roboproject/services/SocketService.java
@@ -49,22 +49,17 @@ public class SocketService extends Service {
 
                 try (Socket steeringSocket = new Socket(ip, port);
                      DataOutputStream dataOutputStream = new DataOutputStream(steeringSocket.getOutputStream());
-                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                     DataOutputStream byteWriter = new DataOutputStream(baos)) {
+                     ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
+                     DataOutputStream byteWriter = new DataOutputStream(byteArrayStream)) {
 
                     while (mainActivity.getToggleButtonStatus()) {
                         ControlData controlData = mainActivity.getControlData();
 
-                        byteWriter.writeFloat(swap(4.8f));
-                        byteWriter.writeFloat(swap(5.7f));
-                        byteWriter.writeFloat(swap(3.9f));
+                        byteWriter.writeFloat(swap(controlData.getAngularVelocity()));
 
-                        controlDataArray = baos.toByteArray();
-
+                        controlDataArray = byteArrayStream.toByteArray();
                         dataOutputStream.write(controlDataArray);
-                        baos.reset();
-
-                        //byteWriter.writeFloat(swap(controlData.getAngularVelocity()));
+                        byteArrayStream.reset();
 
                         Thread.sleep(SOCKET_SLEEP_MILLIS);
                     }
@@ -81,32 +76,28 @@ public class SocketService extends Service {
         }, "robot_control.socket.thread").start();
     }
 
-    private int reverseByteOrder(int i) {
-        return (i & 0xff) << 24 | (i & 0xff00) << 8 | (i & 0xff0000) >> 8 | (i >> 24) & 0xff;
-    }
-
     /**
      * Byte swap a single int value.
      *
      * @param value  Value to byte swap.
-     * @return       Byte swapped representation.
+     * @return Byte swapped representation.
      */
-    public static int swap (int value) {
-        int b1 = (value >>  0) & 0xff;
+    private int swap (int value) {
+        int b1 = (value) & 0xff;
         int b2 = (value >>  8) & 0xff;
         int b3 = (value >> 16) & 0xff;
         int b4 = (value >> 24) & 0xff;
 
-        return b1 << 24 | b2 << 16 | b3 << 8 | b4 << 0;
+        return b1 << 24 | b2 << 16 | b3 << 8 | b4;
     }
 
     /**
      * Byte swap a single float value.
      *
      * @param value  Value to byte swap.
-     * @return       Byte swapped representation.
+     * @return Byte swapped representation.
      */
-    public static float swap (float value) {
+    private float swap (float value) {
         int intValue = Float.floatToIntBits (value);
         intValue = swap (intValue);
         return Float.intBitsToFloat (intValue);
