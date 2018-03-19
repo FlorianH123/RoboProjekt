94ebe80af66eda788ac64b26f827142a83e41208
FlorianH123 <f.hennrich@gmx.de>
2018-03-19 20:52:05 +0100

    Refactor

--- app/src/main/java/com/mobileapplicationdev/roboproject/activities/MainActivity.java
+++ app/src/main/java/com/mobileapplicationdev/roboproject/activities/MainActivity.java
@@ -779,7 +779,6 @@ public class MainActivity extends AppCompatActivity implements SocketService.Cal
     public void setD(float d, int tabId) {
         // TODO löschen
         if (tabId == TAB_ID_2) {
-
             editFrequencyTab2.setText(String.valueOf(d));
         }
 
