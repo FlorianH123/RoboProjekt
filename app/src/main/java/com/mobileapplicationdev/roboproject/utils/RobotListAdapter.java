ba4acb35520e04ea349ceacc2e1f3db16f0f8e97
xOnur <Onur_93@yahoo.de>
2018-03-23 00:33:40 +0100

    scheiße

--- app/src/main/java/com/mobileapplicationdev/roboproject/utils/RobotListAdapter.java
+++ app/src/main/java/com/mobileapplicationdev/roboproject/utils/RobotListAdapter.java
@@ -3,7 +3,6 @@ package com.mobileapplicationdev.roboproject.utils;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 import com.mobileapplicationdev.roboproject.db.DatabaseHelper;
 import com.mobileapplicationdev.roboproject.models.RobotProfile;
@@ -14,16 +13,15 @@ import java.util.ArrayList;
  * Created by Mesut on 23.03.2018.
  */
 
-public class RobotListAdapter extends ArrayAdapter{
-private Context context;
+public class RobotListAdapter extends ArrayAdapter {
+    private Context context;
     private ArrayList<RobotProfile> robotProfiles;
-private DatabaseHelper dbh;
-        }
+    private DatabaseHelper dbh;
 
-private class ViewHolder {
-    TextView robotNameTextView;
-    TextView robotIpTextView;
 
-}
+    public RobotListAdapter(Context context, RobotProfile profile) {
 
+        robotProfiles = new ArrayList<>();
+        robotProfiles.addAll(dbh.getAllProfiles());
+    }
 }
