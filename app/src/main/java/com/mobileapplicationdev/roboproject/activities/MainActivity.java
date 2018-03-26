7e29397f18eaed8fc77c6f7f3341b09cf0801de4
Cedric <cedi.schreiner@web.de>
2018-03-26 19:35:53 +0200

    - profil Liste

--- app/src/main/java/com/mobileapplicationdev/roboproject/activities/MainActivity.java
+++ app/src/main/java/com/mobileapplicationdev/roboproject/activities/MainActivity.java
@@ -253,11 +253,17 @@ public class MainActivity extends AppCompatActivity implements SocketService.Cal
                 return false;
             }
         });
-        Log.d("test", String.valueOf(profileListView.getChildCount()));
-        registerForContextMenu(profileListView);
 
-//        View view = profileListView.getChildgetChildAt(0);
-//        view.setBackgroundColor(getResources().getColor(R.color.profileListBackground));
+        profileListView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                profileListView.removeOnLayoutChangeListener(this);
+
+                View view = profileListView.getChildAt(0);
+                view.setBackgroundColor(getResources().getColor(R.color.profileListBackground));
+            }
+        });
+        registerForContextMenu(profileListView);
     }
 
     /**
